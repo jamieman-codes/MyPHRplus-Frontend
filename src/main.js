cd9ebb2974cd0b7d599c146f69c7b1374b371395
Jamie Stevens <jws1g18@soton.ac.uk>
2021-02-12 13:22:16 +0000

    Basic firebase authentication implementation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase/app";
+import router from "./routes/index";
+import store from "./store";
 
 Vue.config.productionTip = false
 
+const configOptions = {
+    apiKey: "AIzaSyApM4gylqgmwdaBjROAKKTBOAmHQL-QxJ8",
+    authDomain: "myphrplus-frontend.firebaseapp.com",
+    projectId: "myphrplus-frontend",
+    storageBucket: "myphrplus-frontend.appspot.com",
+    messagingSenderId: "125584718852",
+    appId: "1:125584718852:web:3392d8e1e0d4539767c951",
+    measurementId: "G-5JD8MS45V0"
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
