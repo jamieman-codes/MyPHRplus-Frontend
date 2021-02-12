fbbad8f717adcbcefb966c186f9999050a756c6b
Jamie Stevens <jws1g18@soton.ac.uk>
2021-02-12 16:54:57 +0000

    Removed API key

--- src/main.js
+++ src/main.js
@@ -3,21 +3,23 @@ import App from './App.vue'
 import firebase from "firebase/app";
 import router from "./routes/index";
 import store from "./store";
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import { config } from './config.js';
 
 Vue.config.productionTip = false
 
 const configOptions = {
-    apiKey: "AIzaSyApM4gylqgmwdaBjROAKKTBOAmHQL-QxJ8",
+    apiKey: config.firebaseKey,
     authDomain: "myphrplus-frontend.firebaseapp.com",
     projectId: "myphrplus-frontend",
     storageBucket: "myphrplus-frontend.appspot.com",
     messagingSenderId: "125584718852",
-    appId: "1:125584718852:web:3392d8e1e0d4539767c951",
-    measurementId: "G-5JD8MS45V0"
+    appId: "1:125584718852:web:b566d8654413bc4667c951",
+    measurementId: "G-FXXKX0VEKK"
 };
 
 firebase.initializeApp(configOptions);
